f995935fe6a68e819e6a8403212903472759df66
Ogrodowczyk87 <r.ogrodowczyk87@gmail.com>
2025-05-09 17:22:25 +0100

    upadate expirence descriptions

--- src/data/experiencesData.ts
+++ src/data/experiencesData.ts
@@ -1,3 +1,5 @@
+import { Si3M, SiKomoot } from "react-icons/si";
+
 export interface Experience {
     title: string;
     cardTitle: string;
@@ -7,31 +9,31 @@ export interface Experience {
 
 export const experiences: Experience[] = [
     {
-        title: 'Jan 2023 - Jun 2023',
+        title: '20.09.2013 - 30.02.2015',
+        cardTitle: 'Arla Foods',
+        cardSubtitle: 'Skilled Worker',
+        cardDetailedText: 
+            ``,
+    },
+    {
+        title: '1.067-2015 - 30.06.2021',
         cardTitle: 'NFT',
-        cardSubtitle: 'Tech Solutions',
+        cardSubtitle: 'Skilled Worker',
         cardDetailedText:
-            'Worked on building responsive web applications using React and Tailwind CSS. Collaborated with designers and backend developers to deliver high-quality projects.',
+            '',
+
     },
     {
-        title: 'Jan 2023 - Jun 2023',
-        cardTitle: 'EuroPolymers',
-        cardSubtitle: 'Tech Solutions',
+        title: '1.07.2021 - 15.09.2022',
+        cardTitle: 'Euro Polymers Group',
+        cardSubtitle: 'Roofer',
         cardDetailedText:
-            'Worked on building responsive web applications using React and Tailwind CSS. Collaborated with designers and backend developers to deliver high-quality projects.',
+            'Precisely cut roofing materials to fit complex angles and intersections. Managed inventory of supplies and maintained equipment. Applied specialized solutions to ensure smooth finishes. Determined daily material requirements and coordinated logistics. Removed and properly disposed of old materials to maintain clean work sites.',
     },
     {
-        title: 'Jan 2023 - Jun 2023',
-        cardTitle: 'Ryobi',
-        cardSubtitle: 'Tech Solutions',
-        cardDetailedText:
-            'Worked on building responsive web applications using React and Tailwind CSS. Collaborated with designers and backend developers to deliver high-quality projects.',
-    },
-    {
-        title: 'Jul 2023 - Present',
+        title: '20.09.2022 - Present',
         cardTitle: 'Amazon',
-        cardSubtitle: 'Self-Employed',
-        cardDetailedText:
-            'Developed custom websites for small businesses, focusing on performance, SEO, and user experience.',
+        cardSubtitle: 'Skilled Worker',
+        cardDetailedText: 'Responsible for fulfillment operations including picking, packing, and dispatching orders. Ensuring proper inventory management with accurate labeling and storage. Maintaining workplace safety standards and contributing to a fast-paced, efficient work environment.',
     },
 ];
